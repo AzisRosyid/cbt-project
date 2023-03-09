04b3922fe310286281a8d9497a99a807b71c446d
Azis Rosyid <azisrosyid321@gmail.com>
2023-03-10 05:54:08 +0700

    Update

---
+++ public/front-end/layout login baru/1. login/script.js
@@ -0,0 +1,29 @@
+// $('.overlay').on('click', function(e){
+
+// 	console.log('ok');
+
+
+
+// 	e.preventDefault();
+// });
+
+$('.overlay').on('click', function(e) {
+	$('.login').removeClass('z-1');	
+	$('.pKiri').removeClass('pMuncul');
+	$('.pKanan').removeClass('pMuncul');
+	$('.qKiri').addClass('qMuncul');
+	$('.qKanan').addClass('qMuncul');
+
+		e.preventDefault();
+});
+
+$('.over').on('click', function(e) {
+	$('.pKiri').addClass('pMuncul');
+	$('.pKanan').addClass('pMuncul');
+	$('.login').addClass('z-1');
+	$('.qKiri').removeClass('qMuncul');
+	$('.qKanan').removeClass('qMuncul');	
+
+
+	e.preventDefault();
+})
